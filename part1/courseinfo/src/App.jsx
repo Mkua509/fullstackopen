efe899de2abbb2dc78f1288448d52d98b8866316
Mkua509 <maternuskuang@gmail.com>
2026-02-20 00:51:00 +1300

    Completed task 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,10 +6,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   return(
     <div>
-      <p>{props.part} {props.exercise}</p>
+      <Part part={props.part1} exercise={props.exercise1}/>
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
     </div>
   )
 }
@@ -19,7 +29,6 @@ const Total = (props) => {
     <div>
       <p>Number of exercises {props.exercise}</p>
     </div>
-    
   )
 }
 
@@ -35,9 +44,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content 
+      part1={part1} exercise1={exercises1} 
+      part2={part2} exercise2={exercises2} 
+      part3={part3} exercise3={exercises3} 
+      />
       <Total exercise = {exercises1 + exercises2 + exercises3} />
     </div>
   )
